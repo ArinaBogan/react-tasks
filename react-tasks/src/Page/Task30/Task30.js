44bd2c3d07654d93dd83a0fd8235bac5bd481b26
ArinaBogan <arina.bogan@gmail.com>
2024-02-08 20:05:44 +0300

    added react_tasks

--- react-tasks/src/Page/Task30/Task30.js
+++ react-tasks/src/Page/Task30/Task30.js
@@ -2,8 +2,18 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
 function Task30() {
-
-
+    const [inp, setInp] = useState({ name: '', surname: '' });
+    const getData = (e) => {
+        setInp({ ...inp, [e.target.name]: e.target.value })
+    }
+    const showData = () => {
+        try {
+            if (!inp.name || !inp.surname) throw new Error('введите значение');
+            console.log(inp);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
     return (
         <>
             <h1>Задача 30</h1>
@@ -12,7 +22,9 @@ function Task30() {
                 значения 2 input на пустоту.
             </p>
 
-
+            <input name='name' type="text" onChange={getData} />
+            <input name='surname' type="text" onChange={getData} />
+            <button onClick={showData}>click</button>
             <p><Link to='/'>Перейти на главную страницу</Link></p>
         </>);
 }
