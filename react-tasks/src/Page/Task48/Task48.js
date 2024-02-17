9d91417b99881acc03c4d06b9c8d973858f82894
ArinaBogan <arina.bogan@gmail.com>
2024-02-17 22:42:57 +0300

    added react_tasks

--- react-tasks/src/Page/Task48/Task48.js
+++ react-tasks/src/Page/Task48/Task48.js
@@ -4,21 +4,13 @@ import { useState } from 'react';
 function Task48() {
     const [flag, setFlag] = useState(false);
 
-    function clickButton() {
-        if (flag) {
-           setFlag(false) 
-        } else {
-            setFlag(true) 
-        }
-    }
-
     return (
         <>
             <h1>Задача 48</h1>
             <p>Реализовать селектор, где по клику на копку отображается 3 параграфа. При
                 повторном клике параграфы скрываются.</p>
 
-            <button onClick={clickButton}>click</button>
+            <button onClick={() => setFlag(!flag)}>click</button>
             <div>{flag ? <div><p>1</p><p>2</p> <p>3</p></div> : null}</div>
             <p><Link to='/'>Перейти на главную страницу</Link></p>
         </>);
