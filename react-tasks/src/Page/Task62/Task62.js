ec692c2861d448b3d1f2f7a2635242a71f50ec08
ArinaBogan <arina.bogan@gmail.com>
2024-02-29 22:37:07 +0300

    added react_tasks

--- react-tasks/src/Page/Task62/Task62.js
+++ react-tasks/src/Page/Task62/Task62.js
@@ -3,20 +3,23 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function Task62() {
-    const [val, setVal] = useState('');
+    const [val, setVal] = useState(null);
 
     async function getInfo() {
-        const data = await axios.get('http://numbersapi.com/:id');
+        const num = Math.floor(Math.random() * 10)
+        const data = await axios.get(`http://numbersapi.com/:${num}`);
         setVal(data.data)
     }
+    useEffect(() => {
+        console.log(getInfo(val))
+    })
+
     return (
         <>
             <h1>Задача 62</h1>
             <p> Факты с использованием useState и useEffect: http://numbersapi.com/:id. Создайте
                 компонент React, который при первичном рендеринге отправляет запрос к APIс
                 рандомно сгенерированным числом и отображает результат в консоль.</p>
-
-
 
             <p><Link to='/'>Перейти на главную страницу</Link></p>
         </>);
